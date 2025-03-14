b0fd42d67afb328bb96bf1e4237ee03fb1246e3c
Kami Takegami <99816952+KMTB00052571903@users.noreply.github.com>
2025-03-14 11:19:26 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,3 +9,32 @@ customElements.define('article-small',small)
 import wide from "./wide.js";
 
 customElements.define('article-wide',wide)
+
+fetch('./public/info.JSON')
+  .then((response) => response.json()) // Parsear el JSON
+  .then((data) => {
+   
+    const bigContainer = document.querySelector(".bigArticles");
+    const smallContainer = document.querySelector(".smallArticles");
+    const wideContainer = document.querySelector(".wideArticles");
+
+    // Asignar datos a los componentes 
+    const bigArticles = bigContainer.querySelectorAll("article-big");
+    bigArticles.forEach((article, index) => {
+      article.data = data[index]; // Pasar datos del JSON
+    });
+
+    const smallArticles = smallContainer.querySelectorAll("article-small");
+    smallArticles.forEach((article, index) => {
+      
+      article.data = data[index + bigArticles.length];
+    });
+
+   
+    const wideArticles = wideContainer.querySelectorAll("article-wide");
+    wideArticles.forEach((article, index) => {
+   
+      article.data = data[index + bigArticles.length + smallArticles.length];
+    });
+  })
+  .catch((error) => console.error("Error al cargar el JSON:", error));
